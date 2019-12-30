85ddc1deb9cac7fed9c0e4f877eb5819763cdfb1
Dominic <git@msrd0.de>
2019-12-30 23:36:51 +0100

    mjpg streamer

--- bootstrap/system.sh
+++ bootstrap/system.sh
@@ -18,7 +18,7 @@ apk add chrony tzdata
 setup-timezone -z Europe/Berlin
 
 # other stuff
-apk add tmux emacs-nox htop curl wget wiringpi cage mesa-dri-vc4 libinput cmake make qt5-qtbase-dev qt5-qtwayland-dev git usb
+apk add tmux emacs-nox htop curl wget wiringpi cage mesa-dri-vc4 libinput cmake make qt5-qtbase-dev qt5-qtwayland-dev git usbutils
 echo "vc4" >/etc/modules-load.d/vc4.conf
 
 # login
@@ -50,3 +50,47 @@ depend() {
 EOF
 chmod +x /etc/init.d/octoprint
 rc-update add octoprint
+
+# mjpg-streamer
+apk add ffmpeg libjpeg-turbo-dev
+git clone https://github.com/raspberrypi/userland
+cd userland
+sed -i 's/ bash / sh /g' host_applications/linux/apps/raspicam/CMakeLists.txt
+cmake . \
+	-DCMAKE_C_FLAGS="$CFLAGS -D_GNU_SOURCE" \
+	-DARM64=ON \
+	-DCMAKE_BUILD_TYPE=MinSizeRel \
+	-DCMAKE_INSTALL_RPATH=/opt/vc/lib \
+	-DCMAKE_SHARED_LINKER_FLAGS="-Wl,--no-as-needed"
+make install
+cd ..
+git clone https://github.com/jacksonliam/mjpg-streamer
+cd mjpg-streamer/mjpg-streamer-experimental
+cmake . \
+	-DCMAKE_BUILD_TYPE=MinSizeRel \
+	-DCMAKE_SHARED_LINKER_FLAGS="-Wl,--no-as-needed"
+make install
+cd ../..
+rm -rf userland mjpg-streamer
+addgroup -S mjpg
+adduser -H -G mjpg -S -D mjpg
+adduser mjpg video
+cat >/usr/local/bin/mjpg.sh <<EOF
+#!/bin/sh
+set -e
+
+export LD_LIBRARY_PATH=/opt/vc/lib
+mjpg_streamer -o "output_http.so -w /usr/local/share/mjpg-streamer/www/" -i "input_uvc.so -r 1440x1080 -d /dev/video0 -f 15"
+EOF
+cat >/etc/init.d/mjpg-streamer <<EOF
+#!/sbin/openrc-run
+command="/usr/local/bin/mjpg.sh"
+command_user="mjpg"
+command_background="yes"
+pidfile="/run/mjpg-streamer.pid"
+depend() {
+        need net
+}
+EOF
+chmod +x /usr/local/bin/mjpg.sh /etc/init.d/mjpg-streamer
+rc-update add mjpg-streamer
